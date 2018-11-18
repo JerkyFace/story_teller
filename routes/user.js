08dd20b48a06f5d4ac1ba647c93fc1741d0e6a2b
Nayk <nike.bliznyuk@gmail.com>
2018-11-18 21:44:22 +0600

    profile update route (w/o authorization)

--- routes/user.js
+++ routes/user.js
@@ -9,28 +9,49 @@ var express = require('express'),
 router.get('/user/:user_id', (req, res) => {
     User.findById(req.params.user_id, (error, foundUser) => {
         if (!error && foundUser) {
-            Story.find({}, (error, userStories) => {
-                if (!error && userStories) {
-                    let list = userStories.filter(story => {
-                        story.author.username = foundUser.username;
-                    });
-                    console.log(list);
-                    res.render('user/userpage', {
+            Story.find({
+                "author.id": foundUser._id
+            }, (error, userStories) => {
+                if (!error && userStories.length > 0) {
+                    return res.render('user/userpage', {
                         user: foundUser,
-                        userStories: list
+                        userStories: userStories
                     });
                 } else {
-                    res.render('user/userpage', {
-                        user: foundUser
+                    return res.render('user/userpage', {
+                        user: foundUser,
                     });
                 }
             });
         } else {
-            console.log(error);
-            req.flash('error', error.message);
-            res.redirect('/stories');
+            console.log(error.message);
+            req.flash('error', "Something went wrong");
+            return  res.redirect('/stories');
         }
     });
 });
+
+//update profile
+router.put('/user/:user_id', (req, res) => {
+    let id = req.params.user_id;
+    let newData = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        about: req.body.about
+    };
+
+    User.findByIdAndUpdate(id, newData, (error, foundUser) => {
+        if (!error && foundUser) {
+            req.flash('success', "Successfully updated.");
+            return res.redirect(`/user/${id}`);
+        } else {
+            console.log(error.message);
+            req.flash('error', "Unable to update profile.");
+            return res.redirect(`/user/${id}`);
+        }
+    });
+});
+
 
 module.exports = router;
